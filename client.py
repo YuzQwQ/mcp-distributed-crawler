fc08306149a9ad62daba67537c4795a7ab4ec16a
BeiMingSnow <1758786281@qq.com>
2025-04-07 14:33:32 +0800

    MCP client 连接本地server

--- client.py
+++ client.py
@@ -1,8 +1,16 @@
 import asyncio # 让代码支持异步操作
 import os
+import json
+import sys
+from typing import Optional
+from contextlib import AsyncExitStack # 资源管理（确保客户端关闭时释放资源）
+from zoneinfo import available_timezones
+
 from openai import OpenAI
 from dotenv import load_dotenv
-from contextlib import AsyncExitStack # 资源管理（确保客户端关闭时释放资源）
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
 # 加载 .env 文件，确保 API Key 受到保护
 load_dotenv()
@@ -20,28 +28,86 @@ class MCPClient:
             raise ValueError("❌ 未找到 OpenAI API Key，请在 .env 文件中设置OPENAI_API_KEY")
 
         self.client = OpenAI(api_key=self.openai_api_key, base_url=self.base_url)
-    # async def connect_to_mock_server(self):
-    #     """模拟 MCP 服务器的连接（暂不连接真实服务器）"""
-    #     print("✅ MCP 客户端已初始化，但未连接到服务器")
+        # 创建OpenAI client
+        self.session: Optional[ClientSession] = None
+        self.exit_stack = AsyncExitStack()
+
+    async def connect_to_server(self, server_script_path: str):
+        """连接到 MCP 服务器并列出可用工具"""
+        is_python = server_script_path.endswith('.py')
+        is_js = server_script_path.endswith('.js')
+        if not (is_python or is_js):
+            raise ValueError("服务器脚本必须是 .py 或 .js 文件")
+
+        command = "python" if is_python else "node"
+        server_params = StdioServerParameters(
+            command=command,
+            args=[server_script_path],
+            env=None
+        )
+
+        # 启动 MCP 服务器并建立通信
+        stdio_transport = await self.exit_stack.enter_async_context(stdio_client(server_params))
+        self.stdio, self.write = stdio_transport
+        self.session = await self.exit_stack.enter_async_context(ClientSession(self.stdio, self.write))
+
+        await self.session.initialize()
+
+        # 列出 MCP 服务器上的工具
+        response = await self.session.list_tools()
+        tools = response.tools
+        print("\n已连接到服务器，支持以下工具:", [tool.name for tool in tools])
 
     async def process_query(self,query: str) -> str:
-        """调用 OpenAI API 处理用户查询"""
-        messages = [{"role":"system","content":"你是一个猫娘，帮助用户回答问题，多用“喵”当语气词。"}
-                   ,{"role":"user","content":query}]
-        try:
-            # 调用 OpenAI API
-            response = await asyncio.get_event_loop().run_in_executor(
-                None,
-                lambda: self.client.chat.completions.create(
-                    model=self.model,
-                    messages=messages,
-                    max_tokens=1000,
-                    temperature=0.7
-                )
+        """
+        使用大模型处理查询并调用可用的 MCP 工具（Function Calling）
+        """
+        messages = [{"role":"user","content":query}]
+
+        response = await self.session.list_tools()
+
+        available_tools = [{
+            "type": "function",
+            "function":{
+                "name":tool.name,
+                "description":tool.description,
+                "input_schema":tool.inputschema
+            }
+        }for tool in response.tools]
+
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+            tools=available_tools
+        )
+
+        content = response.choices[0]
+        if content.finish_reason == "tool_calls":
+            # 解析工具
+            tool_call = content.message.tool_calls[0]
+            tool_name = tool_call.function.name
+            tool_args = json.loads(tool_call.function.arguments)
+
+            # 执行工具
+            result = await  self.session.call_tool(tool_name,tool_args)
+            print(f"\n\n[Calling tool {tool_name} with args {tool_args}]\n\n")
+
+            # 将模型返回的调用哪个工具数据和工具执行完的数据都存入messages中
+            messages.append(content.message.model_dump())
+            messages.append({
+                "role": "tool",
+                "content": result.content[0].text,
+                "tool_call_id":tool_call.id,
+            })
+
+            # 将上面的结果再返回给大模型用于生产最终的结果
+            response = self.client.chat.completions.create(
+                model=self.model,
+                messages=messages,
             )
             return response.choices[0].message.content
-        except Exception as e:
-            return f"⚠️ 调用 OpenAI API 时出错: {str(e)}"
+
+        return content.message.content
 
 
     async def chat_loop(self):
@@ -65,9 +131,13 @@ class MCPClient:
         await self.exit_stack.aclose()  # 关闭资源管理器
 
 async def main():
+    if len(sys.argv) < 2:
+        print("Usage: python client.py <path_to_server_script>")
+        sys.exit(1)
+
     client = MCPClient()  # 创建 MCP 客户端
     try:
-        # await client.connect_to_mock_server()  # 连接（模拟）服务器
+        await client.connect_to_server(sys.argv[1])  # 连接服务器
         await client.chat_loop()  # 进入聊天循环
     finally:
         await client.cleanup()  # 确保退出时清理资源
